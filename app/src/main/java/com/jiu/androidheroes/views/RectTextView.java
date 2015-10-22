9699e832daa908b503572ec126a2ed2acabfdad1
王九洲 <wangjiuzhou90@gmail.com>
2015-10-22 10:22:02 +0800

    FEATURE  add RectTextView

--- app/src/main/java/com/jiu/androidheroes/views/RectTextView.java
+++ app/src/main/java/com/jiu/androidheroes/views/RectTextView.java
@@ -1,6 +1,8 @@
 package com.jiu.androidheroes.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -11,18 +13,36 @@ public class RectTextView extends TextView {
 
     public RectTextView(Context context) {
         super(context);
+        initView();
     }
 
     public RectTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initView();
     }
 
     public RectTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
-        mPaint1 = new Paint();
-        mPaint1.setColor(getResources().getColor(android.R.color.holo_blue_light));
+        initView();
     }
 
+    private void initView() {
+        mPaint1 = new Paint();
+        mPaint1.setColor(getResources().getColor(android.R.color.holo_blue_light));
+        mPaint1.setStyle(Paint.Style.FILL);
 
+        mPaint2 = new Paint();
+        mPaint2.setColor(Color.YELLOW);
+        mPaint2.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRect(0, 0 , getMeasuredWidth(), getMeasuredHeight(), mPaint1);
+        canvas.drawRect(10 ,10 , getMeasuredWidth() - 10, getMeasuredHeight() - 10, mPaint2);
+        canvas.save();
+        canvas.translate(10, 0);
+        super.onDraw(canvas);
+        canvas.restore();
+    }
 }
