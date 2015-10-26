070d27af51868333334758e80d7d94da4a17ae1d
王九洲 <wangjiuzhou90@gmail.com>
2015-10-26 10:59:16 +0800

    PERF custom attrs fontName

--- app/src/main/java/com/jiu/androidheroes/views/RectTextView.java
+++ app/src/main/java/com/jiu/androidheroes/views/RectTextView.java
@@ -1,11 +1,15 @@
 package com.jiu.androidheroes.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
+
+import com.jiu.androidheroes.R;
 
 public class RectTextView extends TextView {
     private Paint mPaint1;
@@ -13,20 +17,20 @@ public class RectTextView extends TextView {
 
     public RectTextView(Context context) {
         super(context);
-        initView();
+        init(null);
     }
 
     public RectTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initView();
+        init(attrs);
     }
 
     public RectTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initView();
+        init(attrs);
     }
 
-    private void initView() {
+    private void init(AttributeSet attrs) {
         mPaint1 = new Paint();
         mPaint1.setColor(getResources().getColor(android.R.color.holo_blue_light));
         mPaint1.setStyle(Paint.Style.FILL);
@@ -34,12 +38,22 @@ public class RectTextView extends TextView {
         mPaint2 = new Paint();
         mPaint2.setColor(Color.YELLOW);
         mPaint2.setStyle(Paint.Style.FILL);
+
+        if (attrs != null) {
+            TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.RectTextView);
+            String fontName = array.getString(R.styleable.RectTextView_fontName);
+            if (fontName != null) {
+                Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+                setTypeface(typeface);
+            }
+            array.recycle();
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawRect(0, 0 , getMeasuredWidth(), getMeasuredHeight(), mPaint1);
-        canvas.drawRect(10 ,10 , getMeasuredWidth() - 10, getMeasuredHeight() - 10, mPaint2);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint1);
+        canvas.drawRect(10, 10, getMeasuredWidth() - 10, getMeasuredHeight() - 10, mPaint2);
         canvas.save();
         canvas.translate(10, 0);
         super.onDraw(canvas);
